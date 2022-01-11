055cf03c5754224d4315df9efac5fe309dc0004e
yusukematsumoto <93502995+yusuke-rakus@users.noreply.github.com>
2022-01-11 15:30:36 +0900

    reactディレクトリの追加

--- app/src/main/java/com/example/common/CorsConfig.java
+++ app/src/main/java/com/example/common/CorsConfig.java
@@ -16,11 +16,11 @@ public class CorsConfig {
 	@Bean
 	public CorsFilter corsFilter() {
 		List<String> url = new ArrayList<>();
-		url.add("http://localhost:8081");
-		url.add("http://localhost:8000");
-		url.add("http://localhost:8080");
-		url.add("http://vue:8000");
-		url.add("http://vue:8080");
+		url.add("http://localhost:8081/**");
+		url.add("http://localhost:8000/**");
+		url.add("http://localhost:8080/**");
+		url.add("http://vue:8000/**");
+		url.add("http://vue:8080/**");
 
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(url);
