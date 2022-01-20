249f56d978a4d0ea0864d3b1ac443096b9821a82
yusukematsumoto <93502995+yusuke-rakus@users.noreply.github.com>
2022-01-21 00:24:58 +0900

    いいね, いいね解除機能

---
+++ app/src/main/java/com/example/form/ArticleLikeForm.java
@@ -0,0 +1,29 @@
+package com.example.form;
+
+public class ArticleLikeForm {
+
+	private Integer userId;
+	private Integer articleId;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleLikeForm [userId=" + userId + ", articleId=" + articleId + "]";
+	}
+
+}
