42f17bf9f99c6a0c6357dcfc0fc5cc3c2cf86b13
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-21 10:58:16 +0900

    記事投稿serviceの作成

--- app/src/main/java/com/example/service/ArticleService.java
+++ app/src/main/java/com/example/service/ArticleService.java
@@ -1,11 +1,14 @@
 package com.example.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.common.Status;
+import com.example.domain.Article;
 import com.example.form.ArticleCommentForm;
 import com.example.form.ArticleLikeForm;
+import com.example.form.ArticlePostForm;
 import com.example.mapper.ArticleMapper;
 import com.example.response.Response;
 
@@ -47,6 +50,20 @@ public class ArticleService {
 		}
 		return res;
 
+	}
+	
+	/** 記事投稿 */
+	public Response ArticlePost(ArticlePostForm form) {
+		Response res = new Response();
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		try {
+			articleMapper.ArticlePost(article);
+			articleMapper.ArticleTagsPost(article.getId(), form.getTags());
+		} catch (Exception e) {
+			res.setStatus(Status.ERROR.getStatus());
+		}
+		return res;
 	}
 
 }
