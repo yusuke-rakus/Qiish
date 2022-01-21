749c4bc6c13335dd040f32a9e5b23cac117c6a13
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-21 13:49:18 +0900

    フォームの作成

---
+++ app/src/main/java/com/example/form/UserFollowForm.java
@@ -0,0 +1,30 @@
+package com.example.form;
+
+public class UserFollowForm {
+
+	private Integer userInfoId;
+	
+	private Integer followUserInfoId;
+
+	public Integer getUserInfoId() {
+		return userInfoId;
+	}
+
+	public void setUserInfoId(Integer userInfoId) {
+		this.userInfoId = userInfoId;
+	}
+
+	public Integer getFollowUserInfoId() {
+		return followUserInfoId;
+	}
+
+	public void setFollowUserInfoId(Integer followUserInfoId) {
+		this.followUserInfoId = followUserInfoId;
+	}
+
+	@Override
+	public String toString() {
+		return "UserFollowForm [userInfoId=" + userInfoId + ", followUserInfoId=" + followUserInfoId + "]";
+	}
+	
+}
