1db1004ec9cabb162e0619009c5869d175ff4d78
yusukematsumoto <93502995+yusuke-rakus@users.noreply.github.com>
2022-01-21 16:29:32 +0900

    ドメインの修正

--- app/src/main/java/com/example/domain/UserInfo.java
+++ app/src/main/java/com/example/domain/UserInfo.java
@@ -11,90 +11,143 @@ public class UserInfo {
 	private String description;
 	private String image;
 	private List<Integer> follow;
+	private Integer followCount;
 	private List<Integer> follower;
+	private Integer followerCount;
 	private List<Tag> tags;
 	private List<Article> articles;
-	//記事に対するいいね
+	private Integer articleCount;
+	// 記事に対するいいね
 	private List<Likes> likes;
 	private List<Comment> comments;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getEngineerType() {
 		return engineerType;
 	}
+
 	public void setEngineerType(String engineerType) {
 		this.engineerType = engineerType;
 	}
-	public List<Integer> getFollow() {
-		return follow;
-	}
-	public void setFollow(List<Integer> follow) {
-		this.follow = follow;
-	}
-	public List<Integer> getFollower() {
-		return follower;
-	}
-	public void setFollower(List<Integer> follower) {
-		this.follower = follower;
-	}
-	public List<Tag> getTags() {
-		return tags;
-	}
-	public void setTags(List<Tag> tags) {
-		this.tags = tags;
-	}
-	public List<Article> getArticles() {
-		return articles;
-	}
-	public void setArticles(List<Article> articles) {
-		this.articles = articles;
-	}
-	public List<Likes> getLikes() {
-		return likes;
-	}
-	public void setLikes(List<Likes> likes) {
-		this.likes = likes;
-	}
-	public List<Comment> getComments() {
-		return comments;
-	}
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
-	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getImage() {
 		return image;
 	}
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+	public List<Integer> getFollow() {
+		return follow;
+	}
+
+	public void setFollow(List<Integer> follow) {
+		this.follow = follow;
+	}
+
+	public Integer getFollowCount() {
+		return followCount;
+	}
+
+	public void setFollowCount(Integer followCount) {
+		this.followCount = followCount;
+	}
+
+	public List<Integer> getFollower() {
+		return follower;
+	}
+
+	public void setFollower(List<Integer> follower) {
+		this.follower = follower;
+	}
+
+	public Integer getFollowerCount() {
+		return followerCount;
+	}
+
+	public void setFollowerCount(Integer followerCount) {
+		this.followerCount = followerCount;
+	}
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+
+	public Integer getArticleCount() {
+		return articleCount;
+	}
+
+	public void setArticleCount(Integer articleCount) {
+		this.articleCount = articleCount;
+	}
+
+	public List<Likes> getLikes() {
+		return likes;
+	}
+
+	public void setLikes(List<Likes> likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public String toString() {
 		return "UserInfo [id=" + id + ", userName=" + userName + ", email=" + email + ", engineerType=" + engineerType
-				+ ", description=" + description + ", image=" + image + ", follow=" + follow + ", follower=" + follower
-				+ ", tags=" + tags + ", articles=" + articles + ", likes=" + likes + ", comments=" + comments + "]";
+				+ ", description=" + description + ", image=" + image + ", follow=" + follow + ", followCount="
+				+ followCount + ", follower=" + follower + ", followerCount=" + followerCount + ", tags=" + tags
+				+ ", articles=" + articles + ", articleCount=" + articleCount + ", likes=" + likes + ", comments="
+				+ comments + "]";
 	}
-	
+
 }
