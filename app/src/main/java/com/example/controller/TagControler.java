6878e7ef9c9af87e00639e49f8643410706c671b
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-24 20:38:05 +0900

    controllerの作成

---
+++ app/src/main/java/com/example/controller/TagControler.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.response.TagResponse;
+import com.example.service.TagService;
+
+@RestController
+@RequestMapping("/getTag")
+public class TagControler {
+
+	@Autowired
+	private TagService service;
+	
+	@GetMapping("")
+	public TagResponse getTags() {
+		return service.getTags();
+	}
+}
