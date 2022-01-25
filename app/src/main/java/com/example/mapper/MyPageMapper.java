58f07b0989507b849452fb22d97ce77b91d07c29
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-25 12:57:29 +0900

    MyPageのsqlの発行回数の削減

--- app/src/main/java/com/example/mapper/MyPageMapper.java
+++ app/src/main/java/com/example/mapper/MyPageMapper.java
@@ -1,18 +1,11 @@
 package com.example.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.example.domain.Tag;
 import com.example.domain.UserInfo;
 
 @Mapper
 public interface MyPageMapper {
 	
-	/** マイページ */
+	/** マイページ表示 */
 	public UserInfo myPage(Integer userInfoId);
-	
-	/** マイページ(タグ情報) */
-	public List<Tag> getTagInfo(Integer userInfoId);
 }
