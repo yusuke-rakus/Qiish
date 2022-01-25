b0a6885dd30a0fb91ff3ea392629bc7c307ccbd3
kosuke-17 <tamutamu17@icloud.com>
2022-01-25 13:43:05 +0900

    APIよりプロフィールデータ取得

--- next/src/pages/api/fetchData.tsx
+++ next/src/pages/api/fetchData.tsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 // プロフィール情報取得のAPI
 export const fetchProfile = async () => {
-  const res = await axios.get("http://localhost:3001");
-
+  const res = await axios.get("http://localhost:9090/userPage?userInfoId=1");
   return res.data;
 };
 
