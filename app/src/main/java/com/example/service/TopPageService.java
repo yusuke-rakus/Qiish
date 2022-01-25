28ce4f6a1e365eb9863df97dce47a26480051f25
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-25 18:17:17 +0900

    タイトルだけの検索に変更（暫定）

--- app/src/main/java/com/example/service/TopPageService.java
+++ app/src/main/java/com/example/service/TopPageService.java
@@ -39,11 +39,12 @@ public class TopPageService {
 	public SearchResponse searchKeyword(List<String> keywordList) {
 		SearchResponse res = new SearchResponse();
 		try {
-			List<Article> articleList = Stream
-					.concat(topPageMapper.searchKeywordFromTitle(keywordList).stream(),
-							topPageMapper.searchKeywordFromContent(keywordList).stream())
-					.distinct().collect(Collectors.toList());
-			System.out.println(articleList);
+//			List<Article> articleList = Stream
+//					.concat(topPageMapper.searchKeywordFromTitle(keywordList).stream(),
+//							topPageMapper.searchKeywordFromContent(keywordList).stream())
+//					.distinct().collect(Collectors.toList());
+//			System.out.println(articleList);
+			List<Article> articleList = topPageMapper.searchKeywordFromTitle(keywordList);
 			res.setArticleList(articleList);
 		} catch (Exception e) {
 			res.setStatus(Status.ERROR.getStatus());
