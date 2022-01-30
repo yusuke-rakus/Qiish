8cd89321fcb6999eefb4fa1108af27d3cfe71e0f
yusuke-rakus <93502995+yusuke-rakus@users.noreply.github.com>
2022-01-30 13:53:17 +0900

    プロフィール取得時のエラーを修正

--- app/src/main/java/com/example/form/UserEditForm.java
+++ app/src/main/java/com/example/form/UserEditForm.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
---
+++ app/src/main/java/com/example/form/UserPageForm.java
@@ -0,0 +1,20 @@
+package com.example.form;
+
+public class UserPageForm {
+
+	private Integer userInfoId;
+
+	public Integer getUserInfoId() {
+		return userInfoId;
+	}
+
+	public void setUserInfoId(Integer userInfoId) {
+		this.userInfoId = userInfoId;
+	}
+
+	@Override
+	public String toString() {
+		return "UserPageForm [userInfoId=" + userInfoId + "]";
+	}
+
+}
