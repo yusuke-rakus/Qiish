4a7701041dfcf3560dce7b5f1eb91326b5d00181
kosuke-17 <tamutamu17@icloud.com>
2022-01-31 18:30:01 +0900

    記事削除(bodyにguestIdを追加)

--- next/src/pages/api/deleteData.tsx
+++ next/src/pages/api/deleteData.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
+import getCookie from "../../hooks/cookie/handleCookie";
+const guestIdByCookie = getCookie();
 
 export const deleteArticleById = async (articleId: number) => {
   const res = await axios.post("http://localhost:9090/article/delete", {
     articleId: articleId,
-    // guestId:guestId
+    guestId: guestIdByCookie,
   });
   return res;
 };
