c81577a4758706c2d912a1a1743a9aaeeae5f340
kosuke-17 <tamutamu17@icloud.com>
2022-02-05 21:29:26 +0900

    真偽値２パターンを試す

--- next/__test__/hooks/useToggle.test.tsx
+++ next/__test__/hooks/useToggle.test.tsx
@@ -2,11 +2,11 @@ import "@testing-library/jest-dom/extend-expect";
 import { useToggle } from "../../src/hooks";
 import { renderHook, act } from "@testing-library/react-hooks";
 
-test("トグルが存在するかどうか", () => {
+test("useToggleが存在するかどうか", () => {
   expect(useToggle).toBeDefined();
 });
 
-test("Booleanの初期値を反転させる処理", () => {
+test("Booleanの初期値(true)を反転させる処理", () => {
   const { result } = renderHook(() => useToggle(true));
 
   // Boolean反転前, 第一引数のステートをチェック
@@ -17,4 +17,17 @@ test("Booleanの初期値を反転させる処理", () => {
   });
   //Boolean反転後, ステートをチェック
   expect(result.current[0]).toEqual(false);
+});
+
+test("Booleanの初期値(false)を反転させる処理", () => {
+  const { result } = renderHook(() => useToggle(false));
+
+  // Boolean反転前, 第一引数のステートをチェック
+  expect(result.current[0]).toEqual(false);
+  // 第二引数の関数を実行
+  act(() => {
+    result.current[1]();
+  });
+  //Boolean反転後, ステートをチェック
+  expect(result.current[0]).toEqual(true);
 });
