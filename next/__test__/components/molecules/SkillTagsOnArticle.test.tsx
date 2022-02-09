e7793cd0ae51ba37605b2b1f90294a1ffb192abb
kosuke-17 <tamutamu17@icloud.com>
2022-02-09 15:50:55 +0900

    記事技術タグのボタンのテスト

--- next/__test__/components/molecules/SkillTagsOnArticle.test.tsx
+++ next/__test__/components/molecules/SkillTagsOnArticle.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { SkillTagsOnArticle } from "../../../src/components/molecules";
 
@@ -25,9 +25,17 @@ describe("記事技術タグのテスト", () => {
     ],
     onClickTag: mockCallback,
   };
-  it("記事技術タグが表示すること", () => {
+  it("記事技術タグが表示されてボタンがクリックできること", () => {
     render(<SkillTagsOnArticle {...dummyProps} />);
-    expect(screen.getByRole("button", { name: "React" })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "Next" })).toBeInTheDocument();
+
+    expect(screen.getAllByRole("button")).toHaveLength(2);
+
+    let button = screen.getByRole("button", { name: "React" });
+    expect(button).toBeInTheDocument();
+    expect(fireEvent.click(button)).toBe(true);
+
+    button = screen.getByRole("button", { name: "Next" });
+    expect(button).toBeInTheDocument();
+    expect(fireEvent.click(button)).toBe(true);
   });
 });
