a79882ac867f49895fe418cadfab58bfe517baa2
kosuke-17 <tamutamu17@icloud.com>
2022-02-10 15:03:40 +0900

    フォローリストのデータ取得のテスト作成

--- next/__test__/pages/followList.test.tsx
+++ next/__test__/pages/followList.test.tsx
@@ -1,60 +1,55 @@
 import "@testing-library/jest-dom";
 import { render, cleanup, screen } from "@testing-library/react";
 import httpMocks from "node-mocks-http";
-import { context, rest } from "msw";
+import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { GetServerSidePropsContext } from "next";
 import { getServerSideProps } from "../../src/pages/followList";
-import { FollowList } from "../../src/templates";
+const fetchData = {
+  id: 1,
+  userName: "qiish",
+  email: "sample@qiish.com",
+  engineerType: "WEB",
+  description: "hello",
+  image: "",
+  follow: "",
+  followCount: 1,
+  follower: 0,
+  followerCount: 1,
+  tags: [
+    { id: 1, skill: "React", image: null },
+    { id: 2, skill: "Next", image: null },
+  ],
+  articles: 0,
+  articleCount: 0,
+  likes: 0,
+  comments: 0,
+  followStatus: 0,
+};
+const fetchData2 = {
+  id: 2,
+  userName: "zenn",
+  email: "sample@zenn.com",
+  engineerType: "FR",
+  description: "hey",
+  image: "",
+  follow: "",
+  followCount: 1,
+  follower: 0,
+  followerCount: 1,
+  tags: [
+    { id: 1, skill: "React", image: null },
+    { id: 3, skill: "TypeScript", image: null },
+  ],
+  articles: 0,
+  articleCount: 0,
+  likes: 0,
+  comments: 0,
+  followStatus: 0,
+};
 const server = setupServer(
   rest.post("http://localhost:9090/user/followList", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          userName: "qiish",
-          email: "sample@qiish.com",
-          engineerType: "WEB",
-          description: "hello",
-          image: "",
-          follow: "",
-          followCount: 1,
-          follower: 0,
-          followerCount: 1,
-          tags: [
-            { id: 1, skill: "React", image: null },
-            { id: 2, skill: "Next", image: null },
-          ],
-          articles: 0,
-          articleCount: 0,
-          likes: 0,
-          comments: 0,
-          followStatus: 0,
-        },
-        {
-          id: 2,
-          userName: "zenn",
-          email: "sample@zenn.com",
-          engineerType: "FR",
-          description: "hey",
-          image: "",
-          follow: "",
-          followCount: 1,
-          follower: 0,
-          followerCount: 1,
-          tags: [
-            { id: 1, skill: "React", image: null },
-            { id: 3, skill: "TypeScript", image: null },
-          ],
-          articles: 0,
-          articleCount: 0,
-          likes: 0,
-          comments: 0,
-          followStatus: 0,
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([fetchData, fetchData2]));
   })
 );
 // { guestId: '2', articleUserId: '2' }
@@ -82,10 +77,10 @@ afterAll(() => {
 });
 
 describe("フォローリストデータをテスト", () => {
-  it("フォローリストのデータが表示されること", async () => {
+  it("フォローリストのデータが取得されること", async () => {
     const response = await getServerSideProps(ctx);
-    console.log(response);
-
-    // screen.debug();
+    expect(response).toStrictEqual({
+      props: { fallback: { "/followList": [fetchData, fetchData2] } },
+    });
   });
 });
