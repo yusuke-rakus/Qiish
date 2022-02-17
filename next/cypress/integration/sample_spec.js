6cebffc415456091d403153677cf668ae18540a2
kosuke-17 <tamutamu17@icloud.com>
2022-02-17 15:35:46 +0900

    別のサイトで簡単な結合テストを試した

--- next/cypress/integration/sample_spec.js
+++ next/cypress/integration/sample_spec.js
@@ -4,8 +4,20 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+// describe("初めてのテスト", () => {
+//   it("trueが成功するか", () => {
+//     expect(true).to.equal(true);
+//   });
+// });
+
 describe("初めてのテスト", () => {
   it("trueが成功するか", () => {
-    expect(true).to.equal(true);
+    // 指定のURLに訪れる
+    cy.visit("https://www.cypress.io/");
+    // 引数の文字列をクリックする(buttonタグ)
+    cy.contains("Learn more").click();
+    // 引数の文字列が存在するか確かめる
+    cy.contains("A test runner built for humans");
   });
 });
