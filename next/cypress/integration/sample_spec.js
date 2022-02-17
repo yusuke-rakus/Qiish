651af8254b0a5c4130169933b73be4b78e10a717
kosuke-17 <tamutamu17@icloud.com>
2022-02-17 15:20:17 +0900

    cypressを導入し、テストを開始する

---
+++ next/cypress/integration/sample_spec.js
@@ -0,0 +1,11 @@
+// sample_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe("初めてのテスト", () => {
+  it("trueが成功するか", () => {
+    expect(true).to.equal(true);
+  });
+});
