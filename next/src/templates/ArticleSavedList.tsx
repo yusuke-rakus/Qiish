d27c8981c19704e6272c7c757aa3b88c099c46fd
kosuke-17 <tamutamu17@icloud.com>
2022-02-27 09:46:50 +0900

    メインの色を変更

--- next/src/templates/ArticleSavedList.tsx
+++ next/src/templates/ArticleSavedList.tsx
@@ -216,9 +216,6 @@ const ArticleSavedList: React.FC = () => {
 
   return (
     <div>
-      <div className="ml-20 py-3 text-4xl font-semibold text-orange-500">
-        下書き一覧
-      </div>
       {editFlag ? (
         <div>
           <ArticleSave
@@ -231,67 +228,72 @@ const ArticleSavedList: React.FC = () => {
           />
         </div>
       ) : (
-        <div className="flex mt-1 mr-2 bg-white shadow">
-          <div className="w-1/4 bg-orange-100">
-            {articleSavedList &&
-              articleSavedList.articleList.map((articleData: any) => {
-                return (
-                  <div
-                    key={articleData.id}
-                    className="flex px-5 py-3 m-2 bg-white rounded hover:bg-slate-50"
-                  >
-                    <button
-                      className="w-full"
-                      onClick={() => selectArticle(articleData.title)}
+        <div>
+          <div className="ml-20 py-3 text-4xl font-semibold text-sky-400">
+            下書き一覧
+          </div>
+          <div className="flex mt-1 mr-2 bg-white shadow">
+            <div className="w-1/4 bg-sky-100">
+              {articleSavedList &&
+                articleSavedList.articleList.map((articleData: any) => {
+                  return (
+                    <div
+                      key={articleData.id}
+                      className="flex px-5 py-3 m-2 bg-white rounded hover:bg-slate-50"
                     >
-                      <span className="text-black text-xl font-bold">
-                        {articleData.title}
-                      </span>
-                      <div className="flex flex-wrap">
-                        <span className="pr-2 text-xl">
-                          <TagOutlined />
+                      <button
+                        className="w-full"
+                        onClick={() => selectArticle(articleData.title)}
+                      >
+                        <span className="text-black text-xl font-bold">
+                          {articleData.title}
                         </span>
+                        <div className="flex flex-wrap">
+                          <span className="pr-2 text-xl">
+                            <TagOutlined />
+                          </span>
 
-                        {articleData.articleTags.map((tag: any) => {
-                          return (
-                            <div
-                              key={tag.skill}
-                              className={
-                                "m-1 px-3 py-1 bg-orange-400 text-white font-sans text-xs rounded-sm no-underline "
-                              }
-                            >
-                              {tag.skill}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </button>
-                    {articleData.id === articleId && (
-                      <span className="text-center p-2 h-8 w-16 text-xs text-white bg-orange-400 rounded-2xl">
-                        選択中
-                      </span>
-                    )}
-                  </div>
-                );
-              })}
+                          {articleData.articleTags.map((tag: any) => {
+                            return (
+                              <div
+                                key={tag.skill}
+                                className={
+                                  "m-1 px-3 py-1 bg-sky-400 text-white font-sans text-xs rounded-sm no-underline "
+                                }
+                              >
+                                {tag.skill}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </button>
+                      {articleData.id === articleId && (
+                        <span className="text-center p-2 h-8 w-16 text-xs text-white bg-sky-400 rounded-2xl">
+                          選択中
+                        </span>
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="markdown pt-10 pl-10">
+              <ReactMarkdown className="markdown" remarkPlugins={[remarkGfm]}>
+                {content}
+              </ReactMarkdown>
+            </div>
+            <button
+              onClick={setEditFlag}
+              className="text-6xl fixed right-10 bottom-24 shadow-lg rounded-full text-orange-400 hover:text-orange-200"
+            >
+              <PlusCircleOutlined />
+            </button>
+            <button
+              onClick={onDeleteArticle}
+              className="text-5xl fixed right-10 bottom-10 shadow-lg rounded-full text-orange-400 hover:text-orange-200"
+            >
+              <RestOutlined />
+            </button>
           </div>
-          <div className="markdown pt-10 pl-10">
-            <ReactMarkdown className="markdown" remarkPlugins={[remarkGfm]}>
-              {content}
-            </ReactMarkdown>
-          </div>
-          <button
-            onClick={setEditFlag}
-            className="text-6xl fixed right-10 bottom-24 shadow-lg rounded-full text-orange-400 hover:text-orange-200"
-          >
-            <PlusCircleOutlined />
-          </button>
-          <button
-            onClick={onDeleteArticle}
-            className="text-5xl fixed right-10 bottom-10 shadow-lg rounded-full text-orange-400 hover:text-orange-200"
-          >
-            <RestOutlined />
-          </button>
         </div>
       )}
       <Toaster position="top-center" reverseOrder={false} />
